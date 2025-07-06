0eb2e479bdc832598c728c296bf16dfe347f9012
KeyWorksRW <42786259+KeyWorksRW@users.noreply.github.com>
2025-07-06 10:58:34 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# wxFetch
+
+This repository is primarily designed to be used by KeyWorks Software projects. You are welcome to use it yourself, but be aware that the repository is _not_ based on released versions of wxWidgets. It may or may not be current with the current wxWidgets dev tree -- though it will typically be close to current.
+
+This repository is designed to be used by CMake's `FetchContent_Declare` command, and contains a snapshot of the wxWidgets dev source code. Besides eliminating the 10 submodules, it reduces the size and number of files that need to be downloaded as a dependency. For example, the initial creation of this repository had the following changes from the wxWidgets source code"
+
+```
+Original Total: 9,558 files, 666 folders, 155,896,875 bytes
+
+versus 
+
+New Total: 4,147 files, 236 folders, 78,803,757 bytes
+```
+
+You can use this repository as a wxWidgets dependency by placing the following in your `CMakeLists.txt` file:
+
+```
+    FetchContent_Declare(
+        wxWidgets
+        GIT_REPOSITORY "https://github.com/KeyWorksRW/wxFetch.git"
+        GIT_SHALLOW TRUE
+    )
+
+    FetchContent_MakeAvailable( wxWidgets )
+```
+
+Alternatively, this can be added as a submodule or subtree to your own project. The one downside is that `FetchContent_Declare` will stay current with changes in this repository (unless you set FETCHCONTENT_UPDATES_DISCONNECTED), whereas submodule/subtrees have to be manually updated.
+
+See the wxWidgets [README](maintain/wxReadMe.md) file for information about wxWidgets.
---
+++ maintain/cleanup.bat
@@ -0,0 +1,76 @@
+@echo off
+
+@echo This script is used to clean up the wxWidgets repository
+@echo for use with CMake's FetchContent_Declare command.
+@echo.
+@echo It can take a while to run, so please be patient...
+
+@REM This repository is designed to be used with CMake's FetchContent_Declare command. This script
+@REM cleans up the repository by removing files and directories that are not needed by CMake to build
+@REM the various wxWidgets libraries.
+
+@REM Remove unused directories
+
+if not exist "maintain" (
+    cd ..
+)
+
+if exist ".\3rdparty\catch" rd /s /q ".\3rdparty\catch"
+if exist ".\demos" rd /s /q ".\demos"
+if exist ".\distrib" rd /s /q ".\distrib"
+if exist ".\interface" rd /s /q ".\interface"
+if exist ".\lib" rd /s /q ".\libwh *"
+if exist ".\locale" rd /s /q ".\locale"
+if exist ".\misc" rd /s /q ".\misc"
+if exist ".\samples" rd /s /q ".\samples"
+if exist ".\utils" rd /s /q ".\utils"
+if exist ".\src\png\ci" rd /s /q ".\src\png\ci"
+
+@REM Wildcards are not used to avoid the risk of deleting unintended directories.
+
+@echo Removing unused directories...
+
+for /d /r %%d in (android) do if exist "%%d" rd /s /q "%%d"
+for /d /r %%d in (bin) do if exist "%%d" rd /s /q "%%d"
+for /d /r %%d in (doc) do if exist "%%d" rd /s /q "%%d"
+for /d /r %%d in (docs) do if exist "%%d" rd /s /q "%%d"
+for /d /r %%d in (examples) do if exist "%%d" rd /s /q "%%d"
+for /d /r %%d in (iphone) do if exist "%%d" rd /s /q "%%d"
+
+@echo Halfway through removing directories...
+
+for /d /r %%d in (qt) do if exist "%%d" rd /s /q "%%d"
+for /d /r %%d in (man) do if exist "%%d" rd /s /q "%%d"
+for /d /r %%d in (m4) do if exist "%%d" rd /s /q "%%d"
+for /d /r %%d in (scripts) do if exist "%%d" rd /s /q "%%d"
+for /d /r %%d in (test) do if exist "%%d" rd /s /q "%%d"
+for /d /r %%d in (tests) do if exist "%%d" rd /s /q "%%d"
+
+@REM Remove unused files
+
+@echo Removing unused files...
+
+if exist ".\.git-blame-ignore-revs" del ".\.git-blame-ignore-revs"
+if exist ".\Makefile.in" del ".\Makefile.in"
+if exist ".\mkinstalldirs" del ".\mkinstalldirs"
+if exist ".\regen" del ".\regen"
+if exist ".\README-GIT.md" del ".\README-GIT.md"
+
+for /r %%f in (*.ac) do del /q "%%f"
+for /r %%f in (*.m4) do del /q "%%f"
+for /r %%f in (*.mk) do del /q "%%f"
+for /r %%f in (*.mms) do del /q "%%f"
+for /r %%f in (*.mms) do del /q "%%f"
+for /r %%f in (*.pl) do del /q "%%f"
+for /r %%f in (*.py) do del /q "%%f"
+for /r %%f in (*.sh) do del /q "%%f"
+for /r %%f in (*.yaml) do del /q "%%f"
+for /r %%f in (*.yml) do del /q "%%f"
+
+@REM This repository is designed to be pulled in as a dependecy, so there is no use for Visual Studio files.
+
+for /r %%f in (*.vc*) do del /q "%%f"
+
+@REM Use our own README.md file which explains the purpose of this repositry, and
+@REM links to a copy of the wxWidgets readme file (maintain/wxREADME.md).
+copy /y "maintain\README.md" "." >nul
